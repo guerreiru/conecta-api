a3ed4f6240fa3451abb3dcbbc4bd90e5d17bd895
Fernando Guerreiro <fernando.rodrigues@prodap.com.br>
2025-10-14 18:39:26 -0300

    altera a forma de conexão ao banco

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import { DataSource } from "typeorm";
+import { Address } from "../entities/Address";
 import { Category } from "../entities/Category";
 import { City } from "../entities/City";
 import { Company } from "../entities/Company";
@@ -8,17 +9,12 @@ import { Provider } from "../entities/Provider";
 import { Service } from "../entities/Service";
 import { State } from "../entities/State";
 import { User } from "../entities/User";
-import { Address } from "../entities/Address";
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: String(process.env.DB_PASSWORD),
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
   synchronize: true,
   logging: false,
   entities: [
@@ -34,4 +30,5 @@ export const AppDataSource = new DataSource({
   ],
   migrations: [],
   subscribers: [],
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
 });
