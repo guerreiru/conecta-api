8b5d94b1d1ba5924dae01bf3e96ab177bf25b061
Fernando Guerreiro <fernando.rodrigues@prodap.com.br>
2025-10-15 13:08:55 -0300

    feat: retorna name no token

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,15 +1,22 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
+import { UserRole } from "../types/UserRole";
 
 const JWT_SECRET = process.env.ACCESS_TOKEN_SECRET!;
 const REFRESH_SECRET = process.env.REFRESH_TOKEN_SECRET!;
 
-export const generateAccessToken = (userId: string, roles: string[]) => {
-  const payload: JwtPayload = { userId, roles };
+type UserData = {
+  id: string;
+  name: string;
+  roles: UserRole[];
+};
+
+export const generateAccessToken = ({ id, name, roles }: UserData) => {
+  const payload: JwtPayload = { id, name, roles };
   return jwt.sign(payload, JWT_SECRET, { expiresIn: "15m" });
 };
 
-export const generateRefreshToken = (userId: string, roles: string[]) => {
-  const payload: JwtPayload = { userId, roles };
+export const generateRefreshToken = ({ id, name, roles }: UserData) => {
+  const payload: JwtPayload = { id, name, roles };
   return jwt.sign(payload, REFRESH_SECRET, { expiresIn: "7d" });
 };
 
