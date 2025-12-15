8c27311b9e5542c81a3fb556b58c8e0d3b8acce0
Fernando Guerreiro <fernando.rodrigues@prodap.com.br>
2025-12-14 21:33:11 -0300

    feat: update refresh token cookie maxAge to 30 days

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -5,7 +5,7 @@ export const getRefreshTokenCookieOptions = (): CookieOptions => ({
   secure: process.env.NODE_ENV === "production",
   sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
   path: "/",
-  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 dias
+  maxAge: 30 * 24 * 60 * 60 * 1000, // 30 dias
 });
 
 export const getAccessTokenCookieOptions = (): CookieOptions => ({
